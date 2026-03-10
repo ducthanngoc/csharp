41909a1d9edf4ca406666979d9bd12f3a24d7407
ducthanngoc <161787318+ducthanngoc@users.noreply.github.com>
2026-03-10 10:56:19 +0700

    Add files via upload

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Student
+    {
+        public int id 
+        {
+            get; set; }
+        public string name
+        {
+            get; set;
+        }
+        public int age
+        {
+            get; set;
+        }
+        public double score
+        {
+            get; set;
+        }
+    }
+    internal class Program
+    {
+        static List<Student> LocSinhVienTheoDiem(List<Student> dssv, double score)
+        {
+            return dssv.Where(sv => sv.score >= score).ToList();
+        }
+        static List<Student> SapXepTheoDiem(List<Student> dssv, string type)
+        {
+            if (type == "des")
+            {
+                return dssv.OrderByDescending(sv => sv.score).ToList();
+            }
+            else
+            {
+                return dssv.OrderBy(sv => sv.score).ToList();
+            }
+        }
+        static List<Student> TimSinhVienDiemCaoNhat(List<Student> dssv)
+        {
+            double maxScore = dssv.Max(sv => sv.score);
+
+            return dssv.Where(sv => sv.score == maxScore).ToList();
+        }
+        static double TinhDiemTrungBinh(List<Student> dssv)
+        {
+            return dssv.Average(sv => sv.score);
+        }
+        static void Main(string[] args)
+        {
+            List<Student> dssv = new List<Student>();
+            int id;
+            string name;
+            int age;
+            double score;
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Nhap thong tin sinh vien thu {0}", i + 1);
+                
+                Console.Write("Nhap id: ");
+                id = int.Parse(Console.ReadLine());
+                
+                Console.Write("Nhap ten: ");
+                name = Console.ReadLine();
+
+                Console.Write("Nhap tuoi: ");
+                age = int.Parse(Console.ReadLine());
+
+                Console.Write("Nhap diem: ");
+                score = double.Parse(Console.ReadLine());
+                dssv.Add(new Student { id = id, name = name, age = age, score = score});
+            }
+            Console.WriteLine("\nDanh sach sinh vien:");
+
+            foreach (Student sv in dssv)
+            {
+                Console.WriteLine($"{sv.id} - {sv.name} - {sv.age} - {sv.score}");
+            }
+            Console.WriteLine("\nDanh sach sinh vien co diem >=7:");
+            List<Student> ds1 = LocSinhVienTheoDiem(dssv, 7);
+            foreach (Student sv in ds1)
+            {
+                Console.WriteLine($"{sv.id} - {sv.name} - {sv.age} - {sv.score}");
+            }
+            Console.WriteLine("\nDanh sach sinh vien sap xep theo diem giam dan:");
+            List<Student> ds2 = SapXepTheoDiem(dssv, "des");
+            foreach (Student sv in ds2)
+            {
+                Console.WriteLine($"{sv.id} - {sv.name} - {sv.age} - {sv.score}");
+            }
+            ds2 = TimSinhVienDiemCaoNhat(dssv);
+            Console.WriteLine("\nDanh sach sinh vien co diem cao nhat:");
+            foreach (Student sv in ds2)
+            {
+                Console.WriteLine($"{sv.id} - {sv.name} - {sv.age} - {sv.score}");
+            }
+            double diemtb = TinhDiemTrungBinh(dssv);
+            Console.WriteLine($"Diem trung binh cua tat ca sinh vien: {diemtb}");
+        }
+    }
+}
---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    class Item 
+    {
+        public int id 
+        {
+            get; set; } 
+        public string name 
+        { 
+            get; set; } 
+        public string type 
+        { 
+            get; set; } 
+        public double price 
+        {
+            get; set; } 
+    }
+    internal class Program
+    {
+        static List<Item> LocItemTheoGia(List<Item> inventory, double price) 
+        { 
+            return inventory.Where(item => item.price > price).ToList(); 
+        }
+        static List<Item> GetItemsByType(List<Item> inventory, string type)
+        {
+            return inventory.Where(item => item.type == type).ToList();
+        }
+        static List<Item> SapXepTheoGia(List<Item> inventory, string type) 
+        { 
+            if (type == "des") 
+            { 
+                return inventory.OrderByDescending(item => item.price).ToList(); 
+            } 
+            else 
+            { 
+                return inventory.OrderBy(item => item.price).ToList(); 
+            } 
+        }
+        static List<Item> GetMostExpensiveItem(List<Item> inventory)
+        {
+            double maxPrice = inventory.Max(item => item.price); 
+            return inventory.Where(item => item.price == maxPrice).ToList();
+        }
+        static double GetAveragePrice(List<Item> inventory) 
+        { 
+            return inventory.Average(item => item.price); 
+        }
+        static Dictionary<string, List<Item>> GroupItemTheoType(List<Item> inventory)
+        {
+            return inventory
+                .GroupBy(item => item.type)
+                .ToDictionary(group => group.Key, group => group.ToList());
+        }
+        static void Main(string[] args)
+        {
+            List<Item> inventory = new List<Item>();
+            inventory.Add(new Item {id = 1, name = "Sword", type = "Weapon", price = 90});
+            inventory.Add(new Item { id = 2, name = "Axe", type = "Weapon", price = 120 });
+            inventory.Add(new Item { id = 3, name = "Shield", type = "Weapon", price = 70 });
+            inventory.Add(new Item { id = 4, name = "Fire Armor", type = "Armor", price = 200 });
+            inventory.Add(new Item { id = 5, name = "Helmet", type = "Armor", price = 200 });
+            inventory.Add(new Item { id = 6, name = "Bow", type = "Weapon", price = 180 });
+            inventory.Add(new Item { id = 7, name = "Mana Potion", type = "Potions", price = 30 });
+            inventory.Add(new Item { id = 8, name = "Ice Armor", type = "Armor", price = 200 });
+            inventory.Add(new Item { id = 9, name = "Health Potion", type = "Potions", price = 30 });
+            Console.WriteLine("\nDanh sach item trong inventory:");
+            foreach (var item in inventory)
+            {
+                Console.WriteLine($"{item.id} - {item.name} - {item.type} - {item.price}");
+            }
+            Console.WriteLine("\nDanh sach weapon trong inventory:");
+            List<Item> inventory1 = GetItemsByType(inventory, "Weapon");
+            foreach (var item in inventory1)
+            {
+                Console.WriteLine($"{item.id} - {item.name} - {item.type} - {item.price}");
+            }
+            inventory1 = LocItemTheoGia(inventory, 100);
+            Console.WriteLine("\nDanh sach item co gia > 100 trong inventory:");
+            foreach (var item in inventory1)
+            {
+                Console.WriteLine($"{item.id} - {item.name} - {item.type} - {item.price}");
+            }
+            inventory1 = SapXepTheoGia(inventory, "asc");
+            Console.WriteLine("\nDanh sach item sap xep theo gia tang dan trong inventory:");
+            foreach (var item in inventory1)
+            {
+                Console.WriteLine($"{item.id} - {item.name} - {item.type} - {item.price}");
+            }
+            Console.WriteLine("\nDanh sach group item theo type.:");
+            var groups = GroupItemTheoType(inventory);
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key);
+                foreach (var item in group.Value)
+                {
+                    Console.WriteLine($"{item.id} - {item.name} - {item.price}");
+                }
+            }
+        }
+    }
+}
